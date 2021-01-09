499595ae920719e33c66de42a58ba8952f3cf40f
Paulo Vitor Cruz <paulocsvitor@gmail.com>
2021-01-09 18:34:27 -0300

    add global styling for wysiwyg content

--- assets/theme.js
+++ assets/theme.js
@@ -18,6 +18,51 @@ const theme = extendTheme({
         fontSize: "xl",
         fontWeight: "medium"
       },
+      h1: {
+        fontSize: 60,
+        fontFamily: "Bree Serif"
+      },
+      h2: {
+        fontSize: 50,
+        fontFamily: "Bree Serif"
+      },
+      h3: {
+        fontSize: 40,
+        fontFamily: "Bree Serif"
+      },
+      h4: {
+        fontSize: 35,
+        fontFamily: "Bree Serif"
+      },
+      h5: {
+        fontSize: 30,
+        fontFamily: "Bree Serif"
+      },
+      h6: {
+        fontSize: 21,
+        fontFamily: "Bree Serif"
+      },
+      p: {
+        marginBottom: 15,
+      },
+      a: {
+        textDecoration: 'underline',
+        transition: '0.2s all ease-in',
+        _hover: {
+          backgroundColor: primary['900'],
+          color: "white"
+        }
+      },
+      ul: {
+        marginLeft: 20,
+        marginBottom: 15,
+      },
+      blockQuote: {
+        fontStyle: 'italic',
+        fontSize: 25,
+        paddingLeft: 20,
+        borderLeft: `1px solid ${primary['900']}`
+      }
     },
   },
 });
